5418813baa918baa155028efb28a4ba40d29bfc4
softjeard-cmd <softjeard@gmail.com>
2026-01-28 21:55:32 -0500

    Update_1 main.py

--- main.py
+++ main.py
@@ -6,28 +6,34 @@ import os
 from pdf_compress import comprimir_solo_imagenes_pdf
 
 app = FastAPI()
+
 @app.post("/compress")
 async def compress_pdf(
     file: UploadFile = File(...),
     calidad: int = 41,
     escala: float = 0.6
 ):
-    with tempfile.TemporaryDirectory() as tmp:
-        input_path = os.path.join(tmp, file.filename)
-        output_path = os.path.join(tmp, "optimizado.pdf")
+    # archivo de entrada temporal
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as input_tmp:
+        input_tmp.write(await file.read())
+        input_path = input_tmp.name
 
-        with open(input_path, "wb") as f:
-            f.write(await file.read())
+    # archivo de salida temporal
+    output_tmp = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
+    output_path = output_tmp.name
+    output_tmp.close()
 
-        comprimir_solo_imagenes_pdf(
-            input_path,
-            output_path,
-            calidad,
-            escala
-        )
+    comprimir_solo_imagenes_pdf(
+        input_path,
+        output_path,
+        calidad,
+        escala
+    )
 
-        return FileResponse(
-            output_path,
-            media_type="application/pdf",
-            filename="PDF_OPTIMIZADO.pdf"
-        )
+    return FileResponse(
+        output_path,
+        media_type="application/pdf",
+        filename="PDF_OPTIMIZADO.pdf"
+    )
+
+
