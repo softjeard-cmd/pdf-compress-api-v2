f0a6e3626cde281f0df276266121f8253ee8c3ee
softjeard-cmd <softjeard@gmail.com>
2026-01-28 19:22:35 -0500

    First files uploades

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import FileResponse
+import tempfile
+import os
+
+from pdf_compress import comprimir_solo_imagenes_pdf
+
+app = FastAPI()
+@app.post("/compress")
+async def compress_pdf(
+    file: UploadFile = File(...),
+    calidad: int = 41,
+    escala: float = 0.6
+):
+    with tempfile.TemporaryDirectory() as tmp:
+        input_path = os.path.join(tmp, file.filename)
+        output_path = os.path.join(tmp, "optimizado.pdf")
+
+        with open(input_path, "wb") as f:
+            f.write(await file.read())
+
+        comprimir_solo_imagenes_pdf(
+            input_path,
+            output_path,
+            calidad,
+            escala
+        )
+
+        return FileResponse(
+            output_path,
+            media_type="application/pdf",
+            filename="PDF_OPTIMIZADO.pdf"
+        )
---
+++ pdf_compress.py
@@ -0,0 +1,59 @@
+import fitz  # PyMuPDF
+import io
+from PIL import Image
+
+
+def comprimir_solo_imagenes_pdf(
+    entrada,
+    salida,
+    calidad=41,
+    escala=0.60
+):
+    doc = fitz.open(entrada)
+
+    for page in doc:
+        for img in page.get_images(full=True):
+            xref = img[0]
+
+            if calidad == 100 and escala == 1:
+                continue
+
+            try:
+                base = doc.extract_image(xref)
+                img_bytes = base["image"]
+
+                img_pil = Image.open(io.BytesIO(img_bytes))
+
+                if img_pil.width < 50 or img_pil.height < 50:
+                    continue
+
+                if img_pil.mode != "RGB":
+                    img_pil = img_pil.convert("RGB")
+
+                if escala < 1:
+                    new_w = int(img_pil.width * escala)
+                    new_h = int(img_pil.height * escala)
+                    if new_w < 50 or new_h < 50:
+                        continue
+
+                    img_pil = img_pil.resize(
+                        (new_w, new_h),
+                        Image.LANCZOS
+                    )
+
+                buffer = io.BytesIO()
+                img_pil.save(
+                    buffer,
+                    format="JPEG",
+                    quality=calidad,
+                    optimize=True,
+                    subsampling=2
+                )
+
+                page.replace_image(xref, stream=buffer.getvalue())
+
+            except Exception:
+                continue
+
+    doc.save(salida, garbage=4, deflate=True, clean=True)
+    doc.close()
