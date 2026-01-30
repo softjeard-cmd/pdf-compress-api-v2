56987b169c57a8ec3caed642140081993d53cc4e
softjeard-cmd <softjeard@gmail.com>
2026-01-30 18:01:17 -0500

    A ingles pdf_compress.py

--- pdf_compress.py
+++ pdf_compress.py
@@ -6,8 +6,8 @@ from PIL import Image
 def comprimir_solo_imagenes_pdf(
     entrada,
     salida,
-    calidad=41,
-    escala=0.60
+    quality=41,
+    scale=0.60
 ):
     doc = fitz.open(entrada)
 
@@ -15,7 +15,8 @@ def comprimir_solo_imagenes_pdf(
         for img in page.get_images(full=True):
             xref = img[0]
 
-            if calidad == 100 and escala == 1:
+            # no hacer nada si no hay compresión real
+            if quality >= 100 and scale >= 1:
                 continue
 
             try:
@@ -24,15 +25,18 @@ def comprimir_solo_imagenes_pdf(
 
                 img_pil = Image.open(io.BytesIO(img_bytes))
 
+                # ignorar iconos / thumbnails
                 if img_pil.width < 50 or img_pil.height < 50:
                     continue
 
                 if img_pil.mode != "RGB":
                     img_pil = img_pil.convert("RGB")
 
-                if escala < 1:
-                    new_w = int(img_pil.width * escala)
-                    new_h = int(img_pil.height * escala)
+                # escalado
+                if scale < 1:
+                    new_w = int(img_pil.width * scale)
+                    new_h = int(img_pil.height * scale)
+
                     if new_w < 50 or new_h < 50:
                         continue
 
@@ -45,17 +49,20 @@ def comprimir_solo_imagenes_pdf(
                 img_pil.save(
                     buffer,
                     format="JPEG",
-                    quality=calidad,
+                    quality=quality,
                     optimize=True,
                     subsampling=2
                 )
 
                 page.replace_image(xref, stream=buffer.getvalue())
 
-            except Exception:
+            except Exception as e:
+                print("Error procesando imagen - Si ingreso a pdf-compress-api-v2:", e)
                 continue
 
     doc.save(salida, garbage=4, deflate=True, clean=True)
     doc.close()
+
+
 
 
